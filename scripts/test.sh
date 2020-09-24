403363453c78eb3a6124e8a220da86241d362dda
Symblox Dev <symblox@protonmail.com>
2020-09-24 14:23:48 +0800

    Ganache start script parameter modification

--- scripts/test.sh
+++ scripts/test.sh
@@ -27,7 +27,7 @@ start_ganache() {
   if [ "$SOLIDITY_COVERAGE" = true ]; then
     node_modules/.bin/testrpc-sc --gasLimit 0xfffffffffff --port "$ganache_port" > /dev/null &
   else
-    node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff --port "$ganache_port" > /dev/null &
+    node_modules/.bin/ganache-cli -d --gasLimit 0xfffffffffff --port "$ganache_port" > /dev/null &
   fi
 
   ganache_pid=$!
